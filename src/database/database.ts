479a15fde7d4b2b59b6848412dd1c7a61035e70c
Lost Elf <ilef.chebil@insat.ucar.tn>
2024-04-19 23:55:34 +0000

    first commit & schema add

---
+++ src/database/database.ts
@@ -0,0 +1,35 @@
+enum Role {
+    ADMIN = "ADMIN",
+    USER = "USER",
+  }
+  
+  export const users = [
+    {
+      id: "1",
+      name: "Aymen",
+      email: "aymen@gmail.com",
+      role: Role.ADMIN,
+    },
+  ];
+  
+  export const cvs = [
+    {
+      id: "12345",
+      name: "FullStack Developer",
+      age: 41,
+      job: "Freelancer",
+      user: "1",
+      skillIds: ["101", "102"],
+    },
+  ];
+  
+  export const skills = [
+    {
+      id: "101",
+      designation: "JavaScript",
+    },
+    {
+      id: "102",
+      designation: "React",
+    },
+  ];
