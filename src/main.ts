67f720083455b132faff4a9a8a793e9c2661f66c
Omar-Maalej <omar.maalej@insat.ucar.tn>
2024-04-19 23:32:17 +0100

    feat : Query getAllCvs

---
+++ src/main.ts
@@ -0,0 +1,23 @@
+import { createSchema, createYoga } from "graphql-yoga";
+import { createServer } from "http";
+import  { Query }  from "./resolvers/Query";
+const fs = require("fs");
+const path = require("path");
+
+export const schema = createSchema({
+    typeDefs: fs.readFileSync(
+        path.join(__dirname, "schema/schema.graphql"),"utf-8"),
+        resolvers: {
+            Query
+        },
+    });
+
+function main() {
+    const yoga = createYoga({ schema });
+    const server = createServer(yoga);
+    console.log("hello world");
+    server.listen(4000, () => {
+        console.info("Server is running on http://localhost:4000/graphql");
+    });
+}
+main();
--- src/resolvers/Query.ts
+++ src/resolvers/Query.ts
@@ -1,3 +1,30 @@
+import { GraphQLError } from "graphql";
+import { cvs, skills, users } from "../database/database"
+
 export const Query = {
-  
+  getAllCVs: () => {
+    return cvs.map(cv => ({
+      ...cv,
+      user: users.find(user => user.id === cv.user),  
+      skills: cv.skillIds.map(skillId => skills.find(skill => skill.id === skillId)),
+    }));
+  },
+
+  getCVById: (_ : any, { id } : {id : string}) => {
+    const cv= cvs.find((cv) => cv.id === id);
+    console.log(cv);
+    if(cv) return cv;
+    else throw new GraphQLError("CV not found 404 error",
+    {
+        extensions: {
+            http: {
+                status: 404,
+                headers: {
+                "x-custom-header": "some-value",
+                },
+            },
+        }
+    });
+    }
+
 }
