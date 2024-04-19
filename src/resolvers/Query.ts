98750f86f333bb32e36da4a33b7a4837061a685d
Lost Elf <ilef.chebil@insat.ucar.tn>
2024-04-19 23:57:37 +0000

    feat: getAllCvs

--- src/resolvers/Query.ts
+++ src/resolvers/Query.ts
@@ -1,3 +1,13 @@
+import { GraphQLError } from "graphql";
+import { cvs, skills, users } from "../database/database"
+
 export const Query = {
-  
+  getAllCVs: () => {
+    return cvs.map(cv => ({
+      ...cv,
+      user: users.find(user => user.id === cv.user),  
+      skills: cv.skillIds.map(skillId => skills.find(skill => skill.id === skillId)),
+    }));
+  }
+
 }
